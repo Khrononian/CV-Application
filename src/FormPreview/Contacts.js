43b38c29c2bdc96bb56a72036cb19ff4a483185b
Khrononian <Alv.Barnes@gmail.com>
2022-06-26 23:22:18 -0400

    Fixed code

--- src/FormPreview/Contacts.js
+++ src/FormPreview/Contacts.js
@@ -4,11 +4,7 @@ import React, { Component } from 'react'
 class Contacts extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div className='contacts heading'>
-=======
-      <div>
->>>>>>> 65daebb94c44f7ca1296421f8e96ac569913d31c
           <img src={this.props.imageUrl} alt='Avatar' />
           <h3>Contacts</h3>
           <p className='phone'>{this.props.phoneNum}</p>
